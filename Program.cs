78d14c041c3a519a915462f3a16f4120a5cf805a
Esdras Sudário <esdrassudario@gmail.com>
2020-05-09 15:27:57 -0300

    Inserido uma pergunta

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("OI, sou um programa.");
+            Console.WriteLine("Quem é você?");
         }
     }
 }
