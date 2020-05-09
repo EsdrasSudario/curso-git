8570f6861d6ea9f007f2d84200e0b748d9aba706
Esdras Sudário <esdrassudario@gmail.com>
2020-05-09 16:08:30 -0300

    Testando git com o push

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("OI, sou um programa.");
-            Console.WriteLine("Quem é você?");
+            Console.WriteLine("Push");
         }
     }
 }
